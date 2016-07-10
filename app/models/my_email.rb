26ce5dd538db9486529c73bba52c240765e16ebd
Omar Morales <saul090594gmail.com>
2016-07-10 16:56:23 -0500

    unit testing for beta mailer

---
+++ app/models/my_email.rb
@@ -0,0 +1,4 @@
+class MyEmail < ApplicationRecord
+	validates_presence_of :email, message: "Woops! parace que olvidaste colocar el correo."
+	validates_uniqueness_of :email, message: "Este correo ya fue utilizado."
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160710030444) do
+ActiveRecord::Schema.define(version: 20160710212807) do
+
+  create_table "my_emails", force: :cascade do |t|
+    t.string   "email"
+    t.string   "ip"
+    t.integer  "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
