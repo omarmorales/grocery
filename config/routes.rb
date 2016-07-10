de3da836d7c9af9b30e6155be3e4cb119d79b8b2
Omar Morales <saul090594gmail.com>
2016-07-10 17:18:05 -0500

    validate emails and send them

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  post "/emails/create", as: :create_email
+
   authenticated :user do
   	root 'welcome#index'
   end
--- spec/models/my_email_spec.rb
+++ spec/models/my_email_spec.rb
@@ -3,4 +3,14 @@ require 'rails_helper'
 RSpec.describe MyEmail, type: :model do
   it{ should validate_presence_of(:email).with_message"Woops! parace que olvidaste colocar el correo." }
   it{ should validate_uniqueness_of(:email).with_message"Este correo ya fue utilizado." }
+
+  it "should not create with invalid email" do
+  	email = MyEmail.new(email:"saul")
+  	expect(email.valid?).to be_falsy 
+  end
+
+  it "should not create with invalid email" do
+  	email = MyEmail.new(email:"saul@gmail.com")
+  	expect(email.valid?).to be_truthy
+  end
 end
