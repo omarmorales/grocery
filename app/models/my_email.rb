aff5603e81ce2c890c7f81344c360b6c1179f98d
Omar Morales <saul090594gmail.com>
2016-07-11 01:19:27 -0500

    collect emails with ajax

--- app/models/my_email.rb
+++ app/models/my_email.rb
@@ -1,5 +1,5 @@
 class MyEmail < ApplicationRecord
 	validates_presence_of :email, message: "Woops! parace que olvidaste colocar el correo."
 	validates_uniqueness_of :email, message: "Este correo ya fue utilizado."
-	validates_format_of :email, with: Devise::email_regexp
+	validates_format_of :email, with: Devise::email_regexp, message: "No enviaste un correo"
 end
